1723d7d88c93ad07bb5a6481381577df2c796353
qianmi <970435603@qq.com>
2021-05-13 14:43:33 +0800

    second commit

--- src/main/java/com/southwind/utils/Git_test.java
+++ src/main/java/com/southwind/utils/Git_test.java
@@ -2,6 +2,7 @@ package com.southwind.utils;
 
 public class Git_test {
     public static void main(String[] args) {
-        System.out.println("hello Git");
+        System.out.print("hello Git");
+        System.out.print("hello Git_two");
     }
 }
