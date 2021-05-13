62810c0cda4c4659e946aae478ddd42889db82eb
qianmi <970435603@qq.com>
2021-05-13 16:03:18 +0800

    hot-fix commit_1

--- src/main/java/com/southwind/utils/Git_test.java
+++ src/main/java/com/southwind/utils/Git_test.java
@@ -5,5 +5,6 @@ public class Git_test {
         System.out.print("hello Git");
         System.out.print("hello Git_two");
         System.out.print("hello Git_three");
+        System.out.print("hello Git4");
     }
 }
